2e8ceea3fa83e4a1c7f8fb0b1b74b9eacddd1ccd
Camela Kim Quidip <geumjassi74@gmail.com>
2023-12-11 03:17:45 +0800

    added dialog feature for shuffle display

--- frontend/lib/screens/home/home_screen.dart
+++ frontend/lib/screens/home/home_screen.dart
@@ -1,12 +1,15 @@
+import 'dart:math';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:frontend/widgets/alert_dialog.dart';
 import 'package:frontend/widgets/card_feature.dart';
 import 'package:frontend/widgets/custom_appbar.dart';
 import 'package:frontend/widgets/custom_bottombar.dart';
 import 'package:frontend/widgets/follow_restau.dart';
 import 'package:frontend/widgets/personalmenu_card.dart';
-import 'package:frontend/widgets/saved_meals.dart';
+// import 'package:frontend/widgets/saved_meals.dart';
 
 import 'carousel_card.dart';
 
@@ -26,7 +29,9 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -66,33 +71,33 @@ class HomeScreen extends StatelessWidget {
           store: "7-Eleven"),
     ];
 
-    final List<CardFeature> personalMenuCards = [
-      CardFeature(
-        title: 'Title 1',
-        body: 'Body 1',
-        image: imgList[0],
-      ),
-      CardFeature(
-        title: 'Title 2',
-        body: 'Body 2',
-        image: imgList[1],
-      ),
-      CardFeature(
-        title: 'Title 3',
-        body: 'Body 3',
-        image: imgList[2],
-      ),
-      CardFeature(
-        title: 'Title 4',
-        body: 'Body 4',
-        image: imgList[3],
-      ),
-      CardFeature(
-        title: 'Title 5',
-        body: 'Body 5',
-        image: imgList[4],
-      ),
-    ];
+    // final List<CardFeature> personalMenuCards = [
+    //   CardFeature(
+    //     title: 'Title 1',
+    //     body: 'Body 1',
+    //     image: imgList[0],
+    //   ),
+    //   CardFeature(
+    //     title: 'Title 2',
+    //     body: 'Body 2',
+    //     image: imgList[1],
+    //   ),
+    //   CardFeature(
+    //     title: 'Title 3',
+    //     body: 'Body 3',
+    //     image: imgList[2],
+    //   ),
+    //   CardFeature(
+    //     title: 'Title 4',
+    //     body: 'Body 4',
+    //     image: imgList[3],
+    //   ),
+    //   CardFeature(
+    //     title: 'Title 5',
+    //     body: 'Body 5',
+    //     image: imgList[4],
+    //   ),
+    // ];
 
     return Scaffold(
       appBar: CustomAppBar(),
@@ -194,8 +199,9 @@ class HomeScreen extends StatelessWidget {
                       padding: const EdgeInsets.only(left: 10.0),
                       child: FollowedRestaurants()),
                   Container(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: SavedMeals())
+                    padding: const EdgeInsets.only(left: 10.0),
+                    // child: SavedMeals()
+                  )
                 ],
               ),
             ],
@@ -203,16 +209,8 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       extendBody: true,
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   // shape: const RoundedRectangleBorder(
-      //   //     borderRadius: BorderRadius.all(Radius.circular(15.0))),
-      //   child: const Icon(Icons.shuffle),
-      // ),
-      // floatingActionButtonLocation:
-      //     FloatingActionButtonLocation.miniCenterDocked,
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             boxShadow: [BoxShadow(color: Colors.black54, blurRadius: 80)]),
         height: 60,
         child: BottomAppBar(
@@ -250,27 +248,8 @@ class HomeScreen extends StatelessWidget {
               )
             ],
           ),
-          // child:
-          // SizedBox(
-          //     width: double.infinity,
-          //     height: 60,
-          //     child: Row(
-          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //       children: [
-          //         IconButton(
-          //           icon: SvgPicture.asset('hearticon.svg'),
-          //           onPressed: () {},
-          //         ),
-          //         IconButton(
-          //           icon: SvgPicture.asset('addicon.svg'),
-          //           onPressed: () {},
-          //         )
-          //       ],
-          //     )
-          //     ),
         ),
       ),
-
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton.extended(
         label: const Row(
@@ -289,242 +268,17 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Color.fromRGBO(254, 114, 76, 1),
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(17))),
-        onPressed: () {},
+        onPressed: () {
+          int randomIndex = Random().nextInt(personalMenuCards.length);
+          showDialog(
+              context: context,
+              builder: (ctxt) => ShuffleAlertDialog(
+                    image: personalMenuCards[randomIndex].image,
+                    menu: personalMenuCards[randomIndex].title,
+                    store: personalMenuCards[randomIndex].body,
+                  ));
+        },
       ),
-
-      // CustomBottomAppBar(
-      //   cards: featured,
-      // ),
     );
   }
 }
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: const CustomAppBar(),
-//         body: Stack(children: [
-//           SizedBox(
-//             height: MediaQuery.of(context).size.height * 0.7,
-//             child: ClipPath(
-//               clipper: CurvedBackgroundClipper(),
-//               child: Container(
-//                 color: Colors.amber[700],
-//               ),
-//             ),
-//           ),
-//           Column(
-//             mainAxisAlignment: MainAxisAlignment.start,
-//             crossAxisAlignment: CrossAxisAlignment.stretch,
-//             children: [
-//               // Section 1 (Carousel Slider)
-//               Expanded(
-//                 flex: 2,
-//                 child: Stack(
-//                   children: [
-//                     CarouselSlider(
-//                       items: [
-//                         // Replace with carousel items (e.g., images)
-//                         Container(
-//                           color: Colors.blue,
-//                           child: const Center(child: Text('Image 1')),
-//                         ),
-//                         Container(
-//                           color: Colors.green,
-//                           child: const Center(child: Text('Image 2')),
-//                         ),
-//                         Container(
-//                           color: Colors.red,
-//                           child: const Center(child: Text('Image 3')),
-//                         ),
-//                       ],
-//                       options: CarouselOptions(
-//                         height: double.infinity,
-//                         enlargeCenterPage: true,
-//                         enableInfiniteScroll: true,
-//                         autoPlay: true,
-//                       ),
-//                     ),
-//                     Positioned.fill(
-//                       child: Container(
-//                         decoration: const BoxDecoration(
-//                           color: Colors.transparent,
-//                           /* Add any decoration you might need for the carousel */
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               // Section 2 (Other content)
-//               Expanded(
-//                 flex: 3,
-//                 child: Container(
-//                   padding: const EdgeInsets.all(16.0),
-//                   child: const Column(
-//                     children: [
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           Text(
-//                             'Text in First Row',
-//                             style: TextStyle(
-//                               fontSize: 18,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                       // Other content in section 2 goes here
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//               Positioned(
-//                 top: 280, // Adjust the position of the search bar
-//                 left: 0,
-//                 right: 0,
-//                 child: Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//                   child: Container(
-//                     height: 50, // Set your preferred height
-//                     decoration: BoxDecoration(
-//                       color: Colors.white,
-//                       borderRadius: BorderRadius.circular(8.0),
-//                       boxShadow: [
-//                         BoxShadow(
-//                           color: Colors.grey.withOpacity(0.5),
-//                           spreadRadius: 1,
-//                           blurRadius: 3,
-//                           offset: const Offset(0, 2),
-//                         ),
-//                       ],
-//                     ),
-//                     child: const Center(
-//                       child: Text(
-//                           'Search Bar'), // Replace with your search bar widget
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ]));
-//   }
-// }
-
-// final List<String> imgList = [
-//   'https://images.unsplash.com/photo-1658713064117-51f51ecfaf69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-//   'https://images.unsplash.com/photo-1658713064971-5fcef7dfe417?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-//   'https://images.unsplash.com/photo-1616070829624-884057de0b29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60',
-//   'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-//   'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-// ];
-// final List<Widget> imageSliders = imgList
-//     .map((item) => Container(
-//           child: Container(
-//             margin: EdgeInsets.all(5.0),
-//             child: ClipRRect(
-//                 borderRadius: BorderRadius.all(Radius.circular(5.0)),
-//                 child: Stack(
-//                   children: <Widget>[
-//                     Image.network(item, fit: BoxFit.cover, width: 1000.0),
-//                     Positioned(
-//                       bottom: 0.0,
-//                       left: 0.0,
-//                       right: 0.0,
-//                       child: Container(
-//                         decoration: const BoxDecoration(
-//                           gradient: LinearGradient(
-//                             colors: [
-//                               Color.fromARGB(200, 0, 0, 0),
-//                               Color.fromARGB(0, 0, 0, 0)
-//                             ],
-//                             begin: Alignment.bottomCenter,
-//                             end: Alignment.topCenter,
-//                           ),
-//                         ),
-//                         padding: const EdgeInsets.symmetric(
-//                             vertical: 10.0, horizontal: 20.0),
-//                         child: Text(
-//                           'No. ${imgList.indexOf(item)} image',
-//                           style: const TextStyle(
-//                             color: Colors.white,
-//                             fontSize: 20.0,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 )),
-//           ),
-//         ))
-//     .toList();
-
-// return Scaffold(
-//   extendBodyBehindAppBar: true,
-//   appBar: const CustomAppBar(),
-//   bottomNavigationBar: const CustomBottomAppBar(),
-//   body: Stack(
-//         children: [
-//           // First Half: Curved Background with Color
-//           SizedBox(
-//             height: MediaQuery.of(context).size.height * 0.7, // screen height
-//             child: Stack(
-//               children: [
-//                 ClipPath(
-//                   clipper:
-//                       CurvedBackgroundClipper(), // Custom clipper for the curve effect
-//                   child: Container(
-//                     color: Colors.amber[700], // Background color
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-
-//           // Second Half: Content (Carousel)
-//           Positioned(
-//             top: MediaQuery.of(context).size.height *
-//                 0.20, // Adjust this value as needed
-//             left: 0,
-//             right: 0,
-//             child: CarouselSlider(
-//               options: CarouselOptions(
-//                 aspectRatio: 1.5,
-//                 viewportFraction: 0.9,
-//                 enlargeCenterPage: true,
-//                 enlargeStrategy: CenterPageEnlargeStrategy.height,
-//                 enableInfiniteScroll: false,
-//                 initialPage: 2,
-//                 autoPlay: true,
-//               ),
-//               items: imageSliders,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// Custom Clipper for the curved background
-// class CurvedBackgroundClipper extends CustomClipper<Path> {
-//   @override
-//   Path getClip(Size size) {
-//     final path = Path();
-//     path.lineTo(0, size.height * 0.7); // Start from the top-left
-//     path.quadraticBezierTo(size.width / 2, size.height, size.width,
-//         size.height * 0.7); // Create the curve
-//     path.lineTo(size.width, 0); // Close the path
-//     return path;
-//   }
-
-//   @override
-//   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-//     return false;
-//   }
---
+++ frontend/lib/widgets/alert_dialog.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class ShuffleAlertDialog extends StatelessWidget {
+  const ShuffleAlertDialog(
+      {super.key,
+      required this.image,
+      required this.menu,
+      required this.store});
+
+  final String image;
+  final String menu;
+  final String store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: Stack(
+          // fit: StackFit.expand,
+          alignment: Alignment.center,
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                // border: Border.all(color: Colors.green, width: 2.0)
+              ),
+              child: ClipPath(
+                clipper: ClipPathClipper(),
+                child: Container(
+                    decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(21.5),
+                      topRight: Radius.circular(21.5)),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(image),
+                  ),
+                )),
+              ),
+            ),
+
+            // top: 350,
+            // left: 20,
+
+            Positioned(
+              top: 280,
+              left: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(menu,
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(store,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'Montserrat',
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Positioned(
+              top: 300,
+              right: 40,
+              child: Column(
+                children: [SvgPicture.asset('images/Visit.svg')],
+              ),
+            ),
+
+            Positioned(
+              top: 335,
+              left: 20,
+              child: Row(
+                children: [
+                  SvgPicture.asset('images/Trailing Icon.svg'),
+                  const Text('<> km away...',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Color.fromRGBO(254, 114, 76, 1))),
+                  SizedBox(
+                    width: 38,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3.0),
+                        ),
+                        padding: const EdgeInsets.all(8.0),
+                        backgroundColor: HexColor('#FE724C')),
+                    onPressed: () {},
+                    child: const Text(
+                      'Get Directions',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    //
+  }
+}
+
+class ClipPathClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double width = size.width;
+    double height = size.height;
+
+    final path = Path();
+    //(0,0) 1.Point
+    // path.lineTo(0, height); //2.Point
+    // path.quadraticBezierTo(
+    //   width * 0.5, //3.Point --> width * 0.5, height - 100,
+    //   height - 100,
+    //   width, //4.Point --> width, height
+    //   height,
+    // );
+    // path.lineTo(width, 0); //5.Point
+    // path.close();
+    // Path path_0 = Path();
+    path.moveTo(0, size.height * 0.0033333);
+    path.quadraticBezierTo(size.width * 0.0148727, size.height * 0.6094167,
+        size.width * 0.1018000, size.height * 0.6589333);
+    path.cubicTo(
+        size.width * 0.2622455,
+        size.height * 0.6629667,
+        size.width * 0.6531182,
+        size.height * 0.6626958,
+        size.width * 0.8368909,
+        size.height * 0.6639500);
+    path.quadraticBezierTo(size.width * 0.9811455, size.height * 0.6777833,
+        size.width * 1.0018182, size.height * 0.9950000);
+    path.lineTo(size.width * 0.9981818, size.height * 0.0050000);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
