77683dfe6713a5fee8c7c4995d6038ce28ed1b5b
Dean <138504401+dfvargas23@users.noreply.github.com>
2023-12-17 13:54:33 +0800

    Update onboarding_screen.dart

--- frontend/lib/screens/onboarding/onboarding_screen.dart
+++ frontend/lib/screens/onboarding/onboarding_screen.dart
@@ -1,167 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/kakan_logo_icons.dart';
-import 'package:frontend/screens/splash_page.dart';
-// import 'package:kakan_app/kakan_logo_icons.dart';
-// import 'package:kakan_app/screens/splash/splash_page.dart';
+import 'package:kakan_app/kakan_logo_icons.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:frontend/screens/home/home_screen.dart';
+import 'package:frontend/screens/splash/splash_page.dart';
+import 'package:frontend/screens/onboarding/get_started.dart';
 
-class OnboardingScreenFirst extends StatelessWidget {
-  const OnboardingScreenFirst({Key? key}) : super(key: key);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OnboardingScreen(),
+    );
+  }
+}
+
+class OnboardingScreen extends StatelessWidget {
+  final List<OnboardingItem> onboardingScreens = [
+    OnboardingItem(destinationBuilder: () => PersonalMenu()),
+    OnboardingItem(destinationBuilder: () => Discover()),
+    OnboardingItem(destinationBuilder: () => QuickShuffle())
+  ];
 
   @override
   Widget build(BuildContext context) {
-    _navigateToNextScreen(context);
-
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/Background.png'),
-            fit: BoxFit.fill,
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _navigateToNextScreen(BuildContext context) async {
-    await Future.delayed(const Duration(milliseconds: 400));
-
-    // ignore: use_build_context_synchronously
-    Navigator.pushReplacement(
-      context,
-      PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) =>
-            const GetStarted(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          const begin = Offset(1.0, 0.0);
-          const end = Offset.zero;
-          const curve = Curves.ease;
-
-          var tween =
-              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-          var offsetAnimation = animation.drive(tween);
-
-          return SlideTransition(
-            position: offsetAnimation,
-            child: child,
+      body: OnboardingSlider(
+        items: onboardingScreens
+            .map((item) => OnboardingScreenWidget(item: item))
+            .toList(),
+        slidePercent: 70,
+        enableIndicator: true,
+        indicatorSelectedColor: Colors.orange,
+        indicatorUnselectedColor: Colors.grey,
+        onFinished: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
           );
         },
       ),
     );
   }
+
+  OnboardingSlider(
+      {required List<OnboardingScreenWidget> items,
+      required int slidePercent,
+      required bool enableIndicator,
+      required MaterialColor indicatorSelectedColor,
+      required MaterialColor indicatorUnselectedColor,
+      required Null Function() onFinished}) {}
 }
 
-class GetStarted extends StatefulWidget {
-  const GetStarted({Key? key}) : super(key: key);
+class OnboardingScreenWidget extends StatelessWidget {
+  final OnboardingItem item;
 
-  @override
-  _GetStartedState createState() => _GetStartedState();
-}
-
-class _GetStartedState extends State<GetStarted> {
-  bool _showIcon = false;
-  bool _isTapped = false;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(milliseconds: 10), () {
-      setState(() {
-        _showIcon = true;
-      });
-    });
-  }
-
-  void _handleTap() {
-    setState(() {
-      _isTapped = !_isTapped;
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const PersonalMenu(),
-        ),
-      );
-    });
-  }
+  const OnboardingScreenWidget({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/Introduction.png'),
-              fit: BoxFit.fill,
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => item.destinationBuilder(),
           ),
-        ),
-        AnimatedPositioned(
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.easeInOut,
-          bottom: _showIcon ? 320 : -200,
-          left: 0,
-          right: 0,
-          child: const Align(
-            alignment: Alignment.bottomCenter,
-            child: Icon(
-              KakanLogo.kakan,
-              color: Colors.white,
-              size: 250,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 70),
-            child: GestureDetector(
-              onTap: _handleTap,
-              onTapDown: (_) {
-                setState(() {
-                  _isTapped = true;
-                });
-              },
-              onTapUp: (_) {
-                setState(() {
-                  _isTapped = true;
-                });
-              },
-              onTapCancel: () {
-                setState(() {
-                  _isTapped = false;
-                });
-              },
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                width: 200,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: _isTapped
-                      ? Color(0xFFFF9F0E).withOpacity(0.3)
-                      : const Color.fromARGB(255, 254, 254, 255),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Get Started',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      color: Color(0xFFFF9F0E),
-                      decoration: TextDecoration.none,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
+}
+
+class OnboardingItem {
+  final Widget Function() destinationBuilder;
+
+  OnboardingItem({
+    required this.destinationBuilder,
+  });
 }
 
 class PersonalMenu extends StatelessWidget {
@@ -179,7 +103,7 @@ class PersonalMenu extends StatelessWidget {
       child: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/PersonalMenu.png'),
+            image: AssetImage('assets/images/PersonalMenu.png'),
             fit: BoxFit.fill,
             alignment: Alignment.center,
           ),
@@ -305,7 +229,7 @@ class Discover extends StatelessWidget {
       child: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/Discover.png'),
+            image: AssetImage('assets/images/Discover.png'),
             fit: BoxFit.fill,
             alignment: Alignment.center,
           ),
@@ -423,7 +347,7 @@ class QuickShuffle extends StatelessWidget {
       child: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/QuickShuffle.png'),
+            image: AssetImage('assets/images/QuickShuffle.png'),
             fit: BoxFit.fill,
             alignment: Alignment.center,
           ),
